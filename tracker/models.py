1961445e1f4f332dc1e7910f3cae92c733443674
Jolie-B <joliebonner@gmail.com>
2022-11-14 12:29:41 +0000

    day model added

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.template.defaultfilters import join, slugify
+class day(models.Model):
+    date = models.DateField(primary_key=True)
 
-# Create your models here.
+    #symptoms
+    fatigue = models.IntegerField(default=0)
+    nausea = models.IntegerField(default=0)
+    bloated = models.IntegerField(default=0)
+
+    #foods
+    dairy = models.IntegerField(default=0)
+    gluten = models.IntegerField(default=0)
+    sugar = models.IntegerField(default=0)
+
+    daySlug = models.SlugField(unique=True, default='day-')
+
+    def save(self, *args, **kwargs):
+        self.daySlug = (slugify(self.daySlug))
+        super(day, self).save(*args, **kwargs)
+
+    def __str__(self):
+            return self.date    
+
