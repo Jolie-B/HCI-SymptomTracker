15982c340f7533b4cdc9cc3b713c23843a7da8e7
Luke Hopkins <2447720h@student.gla.ac.uk>
2022-11-14 17:14:03 +0000

    page creation

--- tracker/views.py
+++ tracker/views.py
@@ -4,17 +4,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
-
 from tracker.models import *
 from django.db.models import Sum, F
 from django.contrib.auth.models import User
-
 from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tracker index.")
+    return render(request, 'index.html')
 
 
 def callendar(request):
