e6347609d8d164114517f985434afc28759d9b83
Jolie-B <joliebonner@gmail.com>
2022-11-14 12:15:53 +0000

    urls and view for base pages

--- tracker/views.py
+++ tracker/views.py
@@ -6,11 +6,20 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
-from tool.models import *
+from tracker.models import *
 from django.db.models import Sum, F
 from django.contrib.auth.models import User
 
 from django.contrib.auth import authenticate, login, logout
 
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the tracker index.")
 
+
+def callendar(request):
+    return HttpResponse("Hello, world. You're at the tracker callendar.")
+
+
+def stats(request):
+    return HttpResponse("Hello, world. You're at the tracker stats.")
