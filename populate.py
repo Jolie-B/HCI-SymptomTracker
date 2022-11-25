97b1e9102cf791bdd4549d32eb580e005cd84d5c
Jolie-B <joliebonner@gmail.com>
2022-11-25 19:13:09 +0000

    newPopulationScript

--- populate.py
+++ populate.py
@@ -2,7 +2,7 @@ import os
 import string
 import random
 import csv
-
+import json
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'symptomTracker.settings')
@@ -15,62 +15,28 @@ from django.db.models import Q
 from tracker.models import *
 from datetime import date
 from django.contrib.auth.models import User
-
+from django.db.utils import IntegrityError
 
 def populate():
 
     # Day Populate
     
-    dayinfo = [
-        {
-            'date': date(2020, 11, 19),   
-            'fatigue': 0,
-            'nausea': 0,
-            'bloated': 0,
-            'dairy': 5,
-            'gluten': 0,
-            'sugar': 0,
-        },
+    dayinfo = import_days("days.json")
 
-        {
-            'date': date(2020, 11, 20),   
-            'fatigue': 0,
-            'nausea': 3,
-            'bloated': 0,
-            'dairy': 0,
-            'gluten': 3,
-            'sugar': 0,
-        },
-
-        {
-            'date': date(2020, 11, 21),   
-            'fatigue': 0,
-            'nausea': 0,
-            'bloated': 0,
-            'dairy':3,
-            'gluten': 3,
-            'sugar': 3,
-        },
-
-        {
-            'date': date(2020, 11, 22),   
-            'fatigue': 1,
-            'nausea': 2,
-            'bloated': 3,
-            'dairy': 3,
-            'gluten': 4,
-            'sugar': 5,
-        }
-    ]
-    Day = []
-    for day in dayinfo:
-        d= add_day(day['date'], day['fatigue'], day['nausea'], day['bloated'], day['dairy'], day['gluten'], day['sugar'] )
-        Day.append(d)
+    added_days = []
+    for day in dayinfo[0]:
+        if day['date'] not in added_days:
+            add_day(day['date'], day['fatigue'], day['nausea'], day['bloated'], day['dairy'], day['gluten'], day['sugar'])
+            added_days.append(day["date"])
 
 # Model Helper Functions
 
 def add_day(date,fatigue, nausea, bloated, dairy, gluten, sugar):
-    d = Day.objects.get_or_create(date=date, fatigue=fatigue, nausea=nausea, bloated=bloated, dairy=dairy, gluten=gluten, sugar=sugar)[0]
+    try:
+        d = Day.objects.get_or_create(date=date, fatigue=fatigue, nausea=nausea, bloated=bloated, dairy=dairy, gluten=gluten, sugar=sugar)[0]
+    except IntegrityError:
+        d = Day.objects.get(date=date)
+      
     d.date = date
     d.fatigue = fatigue
     d.nausea = nausea
@@ -78,9 +44,27 @@ def add_day(date,fatigue, nausea, bloated, dairy, gluten, sugar):
     d.dairy = dairy
     d.gluten = gluten
     d.sugar = sugar
-    d.save()
 
+    d.save()
     return d
+
+def import_days(filename):
+    days = []
+
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        for item in data:
+            day = {}
+            day['date'] = data[item]['date']
+            day['fatigue'] = data[item]['fatigue']
+            day['dairy'] = data[item]['dairy']
+            day['nausea'] = data[item]['nausea']
+            day['sugar'] = data[item]['sugar']
+            day['gluten'] = data[item]['gluten']
+            day['bloated'] = data[item]['bloated']
+            days.append(day)
+
+    return days,
 
 
 if __name__ == '__main__':
